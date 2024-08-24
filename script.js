b72a9d08b3be43fbdb8cc3d6e7f149e91d7dfa02
Tiago Abraão <125285649+tiagoabra5@users.noreply.github.com>
2024-08-23 21:10:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function transformar (num) {
+    var dec = parseInt(num)
+    var bin = dec.toString(2)
+
+    document.getElementById("resultado").innerText=bin  
+    
+    //console.log(num)
+}
---
+++ teste.js
@@ -0,0 +1,8 @@
+//var x = 5
+//console.log(x.toString(2));
+
+//var dec = document.getElementById ("dec")
+//var resultado = document.querySelector ('span')
+//function transformar () {
+//    return resultado.innerHTML = parseInt(dec.value) / 2
+//}
